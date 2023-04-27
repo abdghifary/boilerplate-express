0fd5b27aad92dfc42100913fc4112d3ae14905db
Agi <abdghifary@gmail.com>
2023-04-27 21:09:10 +0700

    serve Hello Express string

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
 
+app.get('/', function (_, res) {
+  res.send('Hello Express');
+});
+
 console.log('Hello World');
 
 module.exports = app;
